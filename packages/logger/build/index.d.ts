2011850d0b6c6dacba6254567b37db4a4c44608c
George Tzinos <geotzinos@gmail.com>
2020-08-09 23:51:21 +0300

    Added logger build files

---
+++ packages/logger/build/index.d.ts
@@ -0,0 +1,4 @@
+declare const CYAN = "\u001B[36m";
+declare const RED = "\u001B[31m";
+declare const YELLOW = "\u001B[33m";
+declare const log: (color: any, ...args: any) => void;
--- packages/logger/build/index.js
+++ packages/logger/build/index.js
@@ -1,6 +1,8 @@
 "use strict";
-/**
- * Checks if given value is null or undefined or whitespace string
- * @param {string?} value
- */
-exports.isNullOrWhitespace = (value) => value === undefined || value === null || !value.trim();
+const CYAN = '\x1b[36m';
+const RED = '\x1b[31m';
+const YELLOW = '\x1b[33m';
+const log = (color, ...args) => console.log(color + '%s', ...args);
+exports.info = (...args) => log(CYAN, ...args);
+exports.warn = (...args) => log(YELLOW, ...args);
+exports.error = (...args) => log(RED, ...args);
